0c375c8cdd49307ac4e8cf988aff84b070f8c6a9
Chris Wike <chriswike@gmail.com>
2022-08-25 20:42:19 -0500

    Finished all Bonus & Super Bonus missions

--- src/org/launchcode/java/studios/CountingCharacters.java
+++ src/org/launchcode/java/studios/CountingCharacters.java
@@ -1,25 +1,40 @@
 package org.launchcode.java.studios;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CountingCharacters {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws IOException
+    {
 //        String defaultText = "If the product of two terms is zero then common sense says at least one
 //        of the two terms has to be zero to start with. So if you move all the terms over to one side,
 //        you can put the quadratics into a form that can be factored allowing that side of the equation
 //        to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
 //        defaultText.toUpperCase();
-        String defaultText = "Hello!".toUpperCase();
+
+        // Creating a path choosing file from local
+        // directory by creating an object of Path class
+        Path fileName = Path.of("C:\\Projects\\LC101_Homework\\2nd Unit\\java-web-dev-exercises\\src\\org\\launchcode\\java\\exercises\\phrase.txt");
+
+        // Now calling Files.readString() method to
+        // read the file
+        String defaultText = Files.readString(fileName).toUpperCase().replaceAll("[^a-zA-z]", "");
+
+
+        //String defaultText = "Hello!".toUpperCase();
 
         Scanner input = new Scanner(System.in);
         char[] charactersInString;
         HashMap<Character, Integer> characterCount = new HashMap<>();
 
         System.out.println("Please enter text or hit enter to exit");
-        String inputText = input.nextLine().toUpperCase();
+        String inputText = input.nextLine().toUpperCase().replaceAll("[^a-zA-z]", "");
 
         if (inputText.equals("")) {
             charactersInString = defaultText.toCharArray();
